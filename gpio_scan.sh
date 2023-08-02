f846320c531d263416b298af150cba2a137f255f
Jens Glathe <jens.glathe@oldschoolsolutions.biz>
2023-08-02 16:53:37 +0200

    show uptime in seconds as reference

--- gpio_scan.sh
+++ gpio_scan.sh
@@ -5,7 +5,6 @@ if ! mount | grep debugfs > /dev/null; then
 fi
 
 old_state=$(sudo cat /sys/kernel/debug/gpio)
-start_time=$(date +%s%3N)  # Current time in milliseconds
 
 # Print initial states
 echo "$old_state"
@@ -13,19 +12,19 @@ echo "$old_state"
 while true; do
     sleep 0.1
     new_state=$(sudo cat /sys/kernel/debug/gpio)
-    
+
     # Use diff to compare the old and new states
     changes=$(diff <(echo "$old_state") <(echo "$new_state"))
-    
+
     if [ "$changes" != "" ]; then
-        # Calculate elapsed time in milliseconds
-        current_time=$(date +%s%3N)
-        elapsed_time=$(echo "scale=3; ($current_time - $start_time)/1000" | bc)
-        
+        # Calculate elapsed time in seconds
+        current_time=$(cat /proc/uptime | cut -d '.' -f1)
+        elapsed_time=$current_time
+
         # Print elapsed time and changes
-        echo "${elapsed_time}ms:"
+        echo "${elapsed_time}s:"
         echo "$changes"
-        
+
         old_state=$new_state
     fi
 done
