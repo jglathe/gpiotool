8ea448f305b5f186b193a5920d7e075e68bc8374
Jens Glathe <jens.glathe@oldschoolsolutions.biz>
2023-08-02 11:43:50 +0200

    Version using diff to show a smaller changes set
    Signed-off-by: Jens Glathe <jens.glathe@oldschoolsolutions.biz>

--- gpio_scan.sh
+++ gpio_scan.sh
@@ -4,29 +4,28 @@ if ! mount | grep debugfs > /dev/null; then
     sudo mount -t debugfs none /sys/kernel/debug
 fi
 
-# watch -n 0.5 sudo cat /sys/kernel/debug/gpio
+old_state=$(sudo cat /sys/kernel/debug/gpio)
+start_time=$(date +%s%3N)  # Current time in milliseconds
 
-# File to store the last state
-LAST_STATE_FILE="/tmp/last_gpio_state"
-
-# File to store the changes
-OUTPUT_FILE="gpio_changes.txt"
-
-# Get the initial state
-sudo cat /sys/kernel/debug/gpio > $LAST_STATE_FILE
+# Print initial states
+echo "$old_state"
 
 while true; do
-    # Get the current state
-    CURRENT_STATE=$(sudo cat /sys/kernel/debug/gpio)
-
-    # Compare with the last state
-    if ! diff -q $LAST_STATE_FILE <(echo "$CURRENT_STATE") >/dev/null; then
-        # State has changed, record it with a timestamp
-        echo "$(date): GPIO state has changed" >> $OUTPUT_FILE
-        echo "$CURRENT_STATE" >> $OUTPUT_FILE
-        echo "$CURRENT_STATE" > $LAST_STATE_FILE
-    fi
-
-    # Wait a little before checking again
     sleep 0.1
+    new_state=$(sudo cat /sys/kernel/debug/gpio)
+    
+    # Use diff to compare the old and new states
+    changes=$(diff <(echo "$old_state") <(echo "$new_state"))
+    
+    if [ "$changes" != "" ]; then
+        # Calculate elapsed time in milliseconds
+        current_time=$(date +%s%3N)
+        elapsed_time=$(echo "scale=3; ($current_time - $start_time)/1000" | bc)
+        
+        # Print elapsed time and changes
+        echo "${elapsed_time}ms:"
+        echo "$changes"
+        
+        old_state=$new_state
+    fi
 done
